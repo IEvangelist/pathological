ad1609bd69a98b6469d918f01dd6928bb827ed5c
David Pine <david.pine@microsoft.com>
2023-10-12 20:35:25 -0500

    Added more API comments

--- src/tree-builder.ts
+++ src/tree-builder.ts
@@ -13,6 +13,13 @@ export function buildTree(node: FileTreeNode): string {
     return buildFileTree(node, config);
 }
 
+/**
+ * Builds a string representation of a file tree structure starting from a given node.
+ * @param node The root node of the file tree.
+ * @param config The configuration object for the file tree.
+ * @param previousTree The string representation of the previous tree level.
+ * @returns A string representation of the file tree structure.
+ */
 function buildFileTree(
     node: FileTreeNode,
     config: PathologicalConfiguration,
@@ -20,44 +27,65 @@ function buildFileTree(
     const { verticalLine } = config;
 
     if (node.isLeafNotEmpty) {
-        return directoryNotEmpty(previousTree, config);
+        return toLeadingCornerLine(previousTree, config);
     }
 
     let result = "";
 
     if (node.isRoot) {
-        result += directoryItem(true, previousTree, node.name, true, config);
-        previousTree += previousContent(true, verticalLine);
+        result += toSingleLine(true, previousTree, node.name, true, config);
+        previousTree += toLeadingLine(true, verticalLine);
     }
 
     node.children?.forEach((child, index) => {
         const isLastChild = index === node.children!.length - 1;
 
         if (child.isDirectory) {
-            result += directoryItem(isLastChild, previousTree, child.name, true, config);
-            const previousSubtree = previousTree + previousContent(isLastChild, verticalLine);
+            result += toSingleLine(isLastChild, previousTree, child.name, true, config);
+            const previousSubtree = `${previousTree}${toLeadingLine(isLastChild, verticalLine)}`;
             result += buildFileTree(child, config, previousSubtree);
         }
 
         if (child.isFile) {
-            result += directoryItem(isLastChild, previousTree, child.name, false, config);
+            result += toSingleLine(isLastChild, previousTree, child.name, false, config);
         }
     });
 
     return result;
 }
 
-function directoryNotEmpty(previousContent = "", config: PathologicalConfiguration) {
+/**
+ * Returns a string representing the leading line of a folder tree node, including the corner, horizontal line, and open folder icon.
+ * @param previousContent - The content that comes before the leading line.
+ * @param config - The configuration object containing the corner, horizontal line, indent, and open folder icon.
+ * @returns A string representing the leading line of a folder tree node.
+ */
+function toLeadingCornerLine(previousContent = "", config: PathologicalConfiguration) {
     const { corner, horizontalLine, indent, openFolder } = config;
 
     return `${previousContent}${corner}${horizontalLine.repeat(indent - 1)}${openFolder}\n`;
 }
 
-function previousContent(isLast: boolean, verticalLine: string): string {
+/**
+ * Returns a string representing the leading line of a tree node.
+ * @param isLast - A boolean indicating whether the node is the last child of its parent.
+ * @param verticalLine - A string representing the vertical line character used in the tree.
+ * @returns A string representing the leading line of a tree node.
+ */
+function toLeadingLine(isLast: boolean, verticalLine: string): string {
     return `${isLast ? ' ' : verticalLine}${" ".repeat(3)}`;
 }
 
-function directoryItem(
+/**
+ * Returns a single line representation of a directory item, including its name and prefix characters.
+ * @param isLast - Indicates whether the item is the last one in the directory.
+ * @param previousContent - The content that was previously generated for the directory.
+ * @param itemName - The name of the directory item.
+ * @param isDirectory - Indicates whether the item is a directory.
+ * @param config - The configuration object for the tree builder.
+ * @returns A string representing the directory item in a single line.
+ */
+function toSingleLine(
     isLast: boolean,
     previousContent = '',
     itemName = '',
